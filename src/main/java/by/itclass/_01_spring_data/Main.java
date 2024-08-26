03776f306d69ac48b4f16f658febbacf44cff7c8
HOME <1111>
2024-08-26 19:18:02 +0300

    1

---
+++ src/main/java/by/itclass/_01_spring_data/Main.java
@@ -0,0 +1,10 @@
+package by.itclass._01_spring_data;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+public class Main {
+    DataSource dataSource =  new DriverManagerDataSource();
+
+}
